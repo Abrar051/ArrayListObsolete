76684ac82a073ffcea324b6a7dd9cb0f9fdb414d
Abrar051 <75354845+Abrar051@users.noreply.github.com>
2021-01-27 18:16:09 +0600

    Update CallArray.java

--- src/CallArray.java
+++ src/CallArray.java
@@ -23,12 +23,13 @@ public class CallArray {
         }
         System.out.println("Your array size is : "+length);
         System.out.print(" [ ");
-        str.printString(str);
-        str.add(2,"bmhh");
+        str.printString(str, length);
+        //str.add(2,"bmhh");
+        //str.put(3,"hyhyh");
         System.out.print(" ] ");
         System.out.println();
-        str.printString(str);
+        str.printString(str,length+1);
         System.out.println();
-
+        System.out.println("String size is +"+str.getSize());
     }
 }
