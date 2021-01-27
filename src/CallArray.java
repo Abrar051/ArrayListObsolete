a238d36431c300d5611372b3027dcf11ae3b6a98
Abrar051 <75354845+Abrar051@users.noreply.github.com>
2021-01-27 17:13:28 +0600

    Update CallArray.java

--- src/CallArray.java
+++ src/CallArray.java
@@ -5,6 +5,8 @@ public class CallArray {
     public static void main (String args[]){
         Scanner input = new Scanner(System.in);
         DynamicArray str = new DynamicArray();
+        int length=0;
+        // int length=0;
         while (true)
         {
             System.out.println("Enter 1 for input 2 for break : ");
@@ -12,31 +14,21 @@ public class CallArray {
             if (i==1){
                 String arr = input.next();
                 str.add(arr);
+                length++;
             }
             else if (i==2)
             {
                 break;
             }
         }
-        System.out.println("Your array size is : "+(str.data.length));
+        System.out.println("Your array size is : "+length);
         System.out.print(" [ ");
-        for (int i=0;i< str.data.length;i++)
-        {
-            if (str.get(i)==null)
-            {
-                System.out.print("");
-                break;
-            }
-            else{
-                System.out.print(" "+str.get(i));
-            }
-        }
+        str.printString(str);
         str.add(2,"bmhh");
         System.out.print(" ] ");
         System.out.println();
-        for (int i=0;i< str.data.length-1;i++)
-        {
-            System.out.print(" "+str.get(i));
-        }
+        str.printString(str);
+        System.out.println();
+
     }
 }
